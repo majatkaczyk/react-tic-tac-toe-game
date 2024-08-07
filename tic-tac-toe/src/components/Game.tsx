357c73e9d0c4592ec84b2dfe521a41891d944199
Maja <majkatkaczykk@gmail.com>
2024-08-07 12:49:49 +0200

    add game skeleton

--- tic-tac-toe/src/components/Game.tsx
+++ tic-tac-toe/src/components/Game.tsx
@@ -2,12 +2,14 @@ import { useState } from "react";
 import Square from "./Square";
 import { useMachine } from "@xstate/react";
 import { Machine } from "../game-machine/machine";
+import styled from "styled-components";
+import { Board, LeftColumn, Main, RightColumn } from "./styles";
 
 export default function Game() {
   const [state, send] = useMachine(Machine);
 
   return (
-    <>
+    <Main>
       {state.matches("gameOver") && (
         <div>
           {state.hasTag("winner") && <div>Winner: {state.context.winner}</div>}
@@ -15,19 +17,27 @@ export default function Game() {
           <button onClick={() => send({ type: "RESET" })}>Reset</button>
         </div>
       )}
-      <div>
-        {Array(9)
-          .fill(null)
-          .map((_, i) => i)
-          .map((index) => (
-            <Square
-              key={index}
-              index={index}
-              onClickSquare={() => send({ type: "PLAY", value: index })}
-              player={state.context.board[index]}
-            />
-          ))}
-      </div>
-    </>
+      <LeftColumn>
+        <Board>
+          {Array(9)
+            .fill(null)
+            .map((_, i) => i)
+            .map((index) => (
+              <Square
+                key={index}
+                index={index}
+                onClickSquare={() => send({ type: "PLAY", value: index })}
+                player={state.context.board[index]}
+              />
+            ))}
+        </Board>
+      </LeftColumn>
+      <RightColumn>
+        <div>TIC TAC TOE Game</div>
+        <div>Tick's Turn</div>
+        <div>show winner if possible</div>
+        <button>reset</button>
+      </RightColumn>
+    </Main>
   );
 }
---
+++ tic-tac-toe/src/components/styles.ts
@@ -0,0 +1,30 @@
+import styled from "styled-components";
+
+export const Board = styled.div`
+  border-radius: 10px;
+  width: 350px;
+  height: 350px;
+  margin: 0 auto;
+  display: grid;
+  grid-template: repeat(3, 1fr) / repeat(3, 1fr);
+`;
+
+export const Main = styled.div`
+  display: flex;
+  height: 100vh;
+`;
+
+export const LeftColumn = styled.div`
+  flex: 1;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const RightColumn = styled.div`
+  flex: 1;
+  display: flex;
+  justify-content: center;
+  align-items: flex-start;
+  flex-direction: column;
+`;
